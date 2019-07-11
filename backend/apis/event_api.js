320d6ed919eb9b733689f16466bcfabb09e6503d
mishraadityashankar <adityashankarmishra3@gmail.com>
2019-07-11 13:50:04 +0530

    second commit events route changed

--- backend/apis/event_api.js
+++ backend/apis/event_api.js
@@ -3,7 +3,17 @@ const router = express.Router();
 const mongoose= require('mongoose');
 const Event=require('../models/events_model');
 const multer = require('multer');
-const upload = multer({dest:'uploads/'});
+
+
+const storage= multer.diskStorage({
+  destination : (req,file,cb) =>{
+    cb(null,'./uploads/');
+  },
+  filename : (req,file,cb) => {
+    cb(null,file.originalname);
+  }
+});
+const upload = multer({storage:storage});
 
 router.get('/getevent',(req,res) =>{
    Event.find({})
