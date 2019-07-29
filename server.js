c31723a885bef94c0933c8641dd8cd7f32b5b008
mishraadityashankar <adityashankarmishra3@gmail.com>
2019-07-29 13:01:36 +0530

    heroku +4

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
 const path= require('path');
 
-const db= process.env.MONGODB_URI || "mongodb://localhost:27017/Web";
+const db= process.env.MONGOLAB_URI || "mongodb://localhost:27017/Web";
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({
